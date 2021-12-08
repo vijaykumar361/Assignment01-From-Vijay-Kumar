e5024c682d3fb89d9081a86672ee24a9b53703dc
Vijay Kumar <95156170+vijaykumar361@users.noreply.github.com>
2021-12-08 10:09:20 +0500

    Add files via upload

---
+++ assignment01.dart
@@ -0,0 +1,101 @@
+import 'dart:io';
+
+void main() {
+  // print(" ");
+  // print("=====Task_O1=====");
+  // var Name = "Vijay Kumar";
+  // var Product = "Laptop";
+  // var Quantity = "01";
+  // print("Customar Name:_$Name");
+  // print("Product:_$Product");
+  // print("Quantity:_$Quantity");
+
+  // print(" ");
+  // print("=====Task_02=====");
+  // print("Addition");
+  // print("Enter The First Value:");
+  // var first = int.parse(stdin.readLineSync()!);
+  // print("Enter The Second Value:");
+  // var second = int.parse(stdin.readLineSync()!);
+  // int add = first + second;
+  // print("The Addition Of First & Second:_$add");
+  // int sub = first - second;
+  // print("The Subtraction Of First & Second:_$sub");
+  // int multi = first * second;
+  // print("The Multiplication Of First & Second:_$multi");
+  // int dvd = first ~/ second;
+  // print("The Division Of First & Second:_$dvd");
+  // int mod = first % second;
+  // print("The Modulus Of First & Second:_$mod");
+
+  // print(" ");
+  // print("=====Task_03=====");
+  // var rs = 600;
+  // print("Ticket Cost 600 PKR:");
+  // print("How Many Tickets You Want:");
+  // var tic = int.parse(stdin.readLineSync()!);
+  // var TC = rs * tic;
+  // print("Please Pay The Bill:_ $TC");
+
+  // print(" ");
+  // print("=====Task_04=====");
+  // print("Convert Temperature Into Fahrenheit:");
+  // print("Enter Temperature");
+  // var tem = int.parse(stdin.readLineSync()!);
+  // double formula = (tem * 9 / 5) + 32;
+  // print("Temperature Into Farhrenheit: ");
+
+  // print(" ");
+  // print("=====Task_05=====");
+  // var PI1 = 100;
+  // var quant1 = 02;
+  // var PI2 = 200;
+  // var quant2 = 03;
+  // print("Price Of Item_01:_$PI1");
+  // print("Total Quantity Of Item_(01):_$quant1");
+  // int total = PI1 * quant1;
+  // print(" ");
+  // print("Price Of Item_02:_$PI2");
+  // print("Total Quantity Of Item_(02):_$quant1");
+  // int total1 = PI2 * quant2;
+  // print(" ");
+  // print("Pay The Total Bill:_${total + total1}");
+
+  // print(" ");
+  // print("=====Task_06=====");
+
+  // print("RESULT OF STUDENT");
+  // var TM = 850;
+  // var OM = 637;
+  // double percentage = 637 * 100 / 850;
+
+  // print("Total Marks:_ $TM");
+  // print("Obtained Marks:_ $OM");
+  // print("Total Percentage:_ $percentage");
+
+  // print(" ");
+  // print("=====Task_07=====");
+  // int doller = 10;
+  // double rupee = 175.70;
+  // int riyal = 25;
+  // double rupe = 46.85;
+
+  // print("We have 10 Doller:\nAnd 01 Doller = 175.70 Rupees:");
+  // print("Convert 10 Doller Into Rupees:");
+  // double cnvt = rupee * doller;
+  // print("Doller Converted Into Rupees:_$cnvt");
+  // print(" ");
+  // print("We have 25 Riyals:\nAnd 01 Riyal = 46.85 Rupees:");
+  // print("Convert 25 Riyals Into Rupees:");
+  // double cnvrt = rupe * riyal;
+  // print("Doller Converted Into Rupees:_$cnvrt");
+
+  // print(" ");
+  // print("=====Task_08=====");
+  // print("Per Hour Cost Only 100 Rupees");
+  // print("Enter The Hours That You Worked:");
+  // var WH = int.parse(stdin.readLineSync()!);
+  // var cost = 100;
+  // int Salary = WH * cost;
+  // print("Your Total Salary s:_ $Salary");
+}
